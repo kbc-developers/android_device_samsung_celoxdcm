284b83fbd915570cd1f7a0e7816fa4e617c321b6
Nick Reuter <nreuter85@gmail.com>
2013-04-17 20:07:08 -0500

    hercules: update build fingerprint
    Change-Id: I1bd8295cbd66c9406c375850de8b43975c92bdb6

--- ev.mk
+++ ev.mk
@@ -13,13 +13,13 @@ PRODUCT_DEVICE := hercules
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := SAMSUNG-SGH-T989
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.0.4/IMM76D/UVLI4:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.0.4 IMM76D UVLI4 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.1.2/JDQ39/UVMC6:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.1.2 JDQ39 UVMC6 release-keys"
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := potens
 PRODUCT_VERSION_DEVICE_SPECIFIC := p1
 
-PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Galaxy S II\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
+PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your T-Mobile Galaxy S II\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
 
 # Hot reboot
 PRODUCT_PACKAGE_OVERLAYS += \
