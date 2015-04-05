9f4be51172d1f6a949f6996fd6edd83b60d71a88
HomuHomu <ecoco666@gmail.com>
2015-04-05 19:58:50 +0900

    update fingerprint
    Change-Id: I60a5a5e3c08e58a3940dab535a124c2f829a2393

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SC-03D TARGET_DEVICE=SC-03D BUILD_FINGERPRINT="samsung/SC-03D/SC-03D:5.0.2/LRX22G/OMPP5:user/release-keys" PRIVATE_BUILD_DESC="SC-03D-user 5.0.2 LRX22G OMMP5 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SC-03D TARGET_DEVICE=SC-03D BUILD_FINGERPRINT="samsung/SC-03D/SC-03D:5.1/LMY47I/OMPP5:user/release-keys" PRIVATE_BUILD_DESC="SC-03D-user 5.1 LMY47I OMMP5 release-keys"
 
 PRODUCT_NAME := cm_celoxdcm
 PRODUCT_DEVICE := celoxdcm
