2ab2e83ed738524e9423472596451fcd328ca616
Nick Reuter <nreuter85@gmail.com>
2015-04-30 13:27:31 -0500

    hercules: update for vendor changes
     - Cleanup ev.mk a bit more while we are at it.
    
    Change-Id: Ic24e63833b1835418e1927c125971f076c9447d9

--- ev.mk
+++ ev.mk
@@ -1,9 +1,32 @@
-# Inherit from hercules device
+#
+# Copyright (C) 2013 The Evervolv Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Call this first so apn list is actually copied
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/apns.mk)
+
+# Product makefile
 $(call inherit-product, device/samsung/hercules/device.mk)
 
 # Inherit some common evervolv stuff.
-$(call inherit-product, vendor/ev/config/gsm.mk)
-$(call inherit-product, vendor/ev/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common_full_phone.mk)
+
+# Pull all dictionaries
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/dictionaries/intl.mk)
+
+# Bootanimation
+BOOT_ANIMATION_SIZE := wvga
 
 #
 # Setup device specific product configuration.
@@ -19,13 +42,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUI
 PRODUCT_CODENAME := potens
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your T-Mobile Galaxy S II\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
-
-# Hot reboot
-PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/ev/overlay/hot_reboot
-
-# Copy compatible prebuilt files
-PRODUCT_COPY_FILES +=  \
-    vendor/ev/prebuilt/wvga/media/bootanimation.zip:system/media/bootanimation.zip
 
 
