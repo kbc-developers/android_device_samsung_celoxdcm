9f9965f0a5f4a27fb1367a3a2a1a70f169dd5208
bryan2894 <bryan2894@gmail.com>
2015-11-12 23:32:35 -0500

    skyrocket: update makefile
    Change-Id: I6685020a3e13398a2a320cfe7afb35c21323f7ca

--- cm.mk
+++ cm.mk
@@ -6,6 +6,10 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+PRODUCT_DEVICE := skyrocket
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := SGH-I727
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 TARGET_DEVICE=SGH-I727 BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.1.2/JZO54K/I727UCMC1:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.1.2 JZO54K I727UCMC1 release-keys"
 
 PRODUCT_NAME := cm_skyrocket
