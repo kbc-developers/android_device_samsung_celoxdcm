0b55bc9a5fc154cc407e66dfeabe842e97925af0
bryan2894 <bryan2894@gmail.com>
2016-08-11 16:05:06 -0400

    celox: set custom dalvik heap properties
    * Based on values from Intel's memory tuning guide
    * https://01.org/android-ia/user-guides/android-memory-tuning-android-5.0-and-5.1
    
    Change-Id: Ic70334db211c9bd6cfa6e028e90c612131718fd0

--- device.mk
+++ device.mk
@@ -23,8 +23,6 @@ $(call inherit-product-if-exists, vendor/samsung/celox/celox-vendor.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 
-$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
-
 # Bluetooth
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/firmware/bcm4330B1.hcd:system/etc/firmware/bcm4330B1.hcd
@@ -32,6 +30,15 @@ PRODUCT_COPY_FILES += \
 # Bootanimation
 TARGET_SCREEN_WIDTH := 480
 TARGET_SCREEN_HEIGHT := 800
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=64m \
+    dalvik.vm.heapsize=174m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=2m
 
 # Doze
 PRODUCT_PACKAGES += \
