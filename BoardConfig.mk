7addb84142a14a1b130ca39aaa23703980220b2a
bryan2894 <bryan2894@gmail.com>
2016-11-20 21:45:35 -0500

    celox: don't specify toolchain
    Change-Id: Ib0150e83f942f2b4f04a328edbe98a771d493efc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,6 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
 TARGET_KERNEL_CONFIG := celox_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8660-common
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
