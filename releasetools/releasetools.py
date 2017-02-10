39f1e8ee5092a759692ae6b583ec395e2b6c35f6
HomuHomu <ecoco666@gmail.com>
2017-02-11 00:46:21 +0900

    Revert "celox: add assertions from msm8660"
    This reverts commit 79211258c9926884b6f7f8742a90a28ebe5efc2f.

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -13,31 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import common
-import struct
 
 """Custom OTA commands for celox devices"""
 
-def FullOTA_Assertions(info):
-	info.script.AppendExtra("ifelse(!is_mounted(\"/data\"), run_program(\"/sbin/busybox\", \"mount\", \"/data\"));")
-	info.script.AppendExtra("ifelse(!is_mounted(\"/system\"), run_program(\"/sbin/busybox\", \"mount\", \"/system\"));")
-	info.script.AppendExtra(
-		('package_extract_file("install/bin/partitioncheck.sh", "/tmp/partitioncheck.sh");\n'
-		'set_metadata("/tmp/partitioncheck.sh", "uid", 0, "gid", 0, "mode", 0777);'))
-	info.script.AppendExtra('assert(run_program("/tmp/partitioncheck.sh") == 0);')
-
-def FullOTA_PostValidate(info):
-	# run e2fsck
-	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p25");');
-	# resize2fs: run and delete
-	info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/mmcblk0p25");');
-	# run e2fsck
-	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p25");');
-
 def FullOTA_InstallEnd(info):
-	info.script.Mount("/system")
-	info.script.AppendExtra('ifelse(is_substring("SGH-T989", getprop("ro.product.device")) || is_substring("SGH-T989", getprop("ro.build.product")) || is_substring("hercules", getprop("ro.product.device")) || is_substring("hercules", getprop("ro.build.product")), run_program("/sbin/sh", "-c", "busybox cp /system/lib/hercules/* /system/lib/"));')
-	info.script.AppendExtra('ifelse(is_substring("SGH-T989", getprop("ro.product.device")) || is_substring("SGH-T989", getprop("ro.build.product")) || is_substring("hercules", getprop("ro.product.device")) || is_substring("hercules", getprop("ro.build.product")), run_program("/sbin/sh", "-c", "busybox echo SGH-T989 > /system/variant.prop"));')
-	info.script.AppendExtra('ifelse(is_substring("SGH-I727", getprop("ro.product.device")) || is_substring("SGH-I727", getprop("ro.build.product")) || is_substring("skyrocket", getprop("ro.product.device")) || is_substring("skyrocket", getprop("ro.build.product")), run_program("/sbin/sh", "-c", "busybox echo SGH-I727 > /system/variant.prop"));')
-	info.script.AppendExtra('delete_recursive("/system/lib/hercules/");')
-	info.script.Unmount("/system")
+  info.script.Mount("/system")
+  info.script.AppendExtra('ifelse(is_substring("SGH-T989", getprop("ro.product.device")) || is_substring("SGH-T989", getprop("ro.build.product")) || is_substring("hercules", getprop("ro.product.device")) || is_substring("hercules", getprop("ro.build.product")), run_program("/sbin/sh", "-c", "busybox cp /system/lib/hercules/* /system/lib/"));')
+  info.script.AppendExtra('ifelse(is_substring("SGH-T989", getprop("ro.product.device")) || is_substring("SGH-T989", getprop("ro.build.product")) || is_substring("hercules", getprop("ro.product.device")) || is_substring("hercules", getprop("ro.build.product")), run_program("/sbin/sh", "-c", "busybox echo SGH-T989 > /system/variant.prop"));')
+  info.script.AppendExtra('ifelse(is_substring("SGH-I727", getprop("ro.product.device")) || is_substring("SGH-I727", getprop("ro.build.product")) || is_substring("skyrocket", getprop("ro.product.device")) || is_substring("skyrocket", getprop("ro.build.product")), run_program("/sbin/sh", "-c", "busybox echo SGH-I727 > /system/variant.prop"));')
+  info.script.AppendExtra('delete_recursive("/system/lib/hercules/");')
+  info.script.Unmount("/system")
